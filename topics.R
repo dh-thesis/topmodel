a942b60386d2bea300c9d340ff38b2bc8ca1bd9a
herreio <mail@herre.io>
2019-06-19 00:37:39 +0200

    process mpi titles from year 2018

--- topics.R
+++ topics.R
@@ -2,12 +2,16 @@ source("src/reader.R")
 source("src/modeling.R")
 
 # set path to data files
-fp <- '../pubdata/data/corpus/titles/cat--deu'
-out <- './docs/vis/deu/cat_t100'
-k <- 100
+# fp <- '../pubdata/data/corpus/titles/cat--deu'
+# fp <- '../pubdata/data/corpus/titles/mpis--eng--years'
+fp <- './data/mpis--eng/201_years/2018'
+out <- './docs/vis/eng/mpi2018_t20'
+k <- 20
+# pattern <- 'eng_201'
+pattern <- '.txt'
 
 # get document term matrix (dtm)
-dtm <- read.data(fp)
+dtm <- read.data(fp, fp.pattern = pattern)
 
 # fit model for documents of dtm
 topmod <- fit.model(dtm, k)
