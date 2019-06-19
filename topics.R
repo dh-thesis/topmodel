1c7b2dfc0ed35109e3a09f316807291bcc6a237c
herreio <mail@herre.io>
2019-06-19 17:13:08 +0200

    model 50 topics for mpi2016 publications

--- topics.R
+++ topics.R
@@ -7,10 +7,10 @@ source("src/modeling.R")
 fp <- './data/mpis--eng/201_years/2016'
 
 # set path for visualization output
-out <- './docs/vis/eng/mpi2016_t20'
+out <- './docs/vis/eng/mpi2016_t50'
 
 # number of topics
-k <- 20
+k <- 50
 
 # pattern to filter data files
 pattern <- '.txt' # 'eng_201'
