b2560ad6cd6c501a52ce82b28b9a51428282dfe5
herreio <mail@herre.io>
2019-10-04 14:34:45 +0200

    src/modeling.R del empty spaces

--- src/modeling.R
+++ src/modeling.R
@@ -21,7 +21,7 @@ fit.model <- function(doc_term, k) {
 }
 
 model.vis <- function(fitted.model, doc_term, vis.dir="./public/vis") {
-    
+
     print("set variables...")
 
     phi <- as.matrix(posterior(fitted.model)$terms)
@@ -30,12 +30,12 @@ model.vis <- function(fitted.model, doc_term, vis.dir="./public/vis") {
     vocab <- colnames(phi)
 
     term_freq <- slam::col_sums(doc_term)
-    
+
     # while calculating model based on institutes' titles -->
     # Error in stats::cmdscale(dist.mat, k = 2) :
     # NA values not allowed in 'd' 
     # svd_tsne <- function(x) tsne::tsne(svd(x)$u)
-    
+
     print("create visualization...")
 
     json_lda <- LDAvis::createJSON(phi = phi,
@@ -44,11 +44,11 @@ model.vis <- function(fitted.model, doc_term, vis.dir="./public/vis") {
                                    # mds.method = svd_tsne,
                                    doc.length = as.vector(table(doc_term$i)),
                                    term.frequency = term_freq)
-    
+
     print("start server...")
-    
+
     serVis(json_lda, out.dir = vis.dir, open.browser = TRUE, encoding="UTF-8")
-    
+
     print("success!")
 
 }
